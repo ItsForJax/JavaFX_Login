c0cbb46fb0149096d09ef56c3efa1e9eee2f2890
jonathandavid723 <108653099+jonathandavid723@users.noreply.github.com>
2024-05-29 13:04:22 +0800

    Updated dependencies Rename SceneController.java to MainController.java Creation of Chat using ListView in progress

--- src/main/java/com/epds/javafx_login/entities/User.java
+++ src/main/java/com/epds/javafx_login/entities/User.java
@@ -2,10 +2,21 @@ package com.epds.javafx_login.entities;
 
 public class User {
 
+    private int id;
+
     private String name;
 
-    public User(String name) {
+    public User(int id, String name) {
+        this.id = id;
         this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
--- src/main/java/com/epds/javafx_login/utilities/controllers/ChatController.java
+++ src/main/java/com/epds/javafx_login/utilities/controllers/ChatController.java
@@ -2,11 +2,16 @@ package com.epds.javafx_login.utilities.controllers;
 
 import com.epds.javafx_login.entities.ChatMessage;
 import com.epds.javafx_login.entities.User;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class ChatController {
@@ -24,11 +29,37 @@ public class ChatController {
     private Button send_chat_button;
 
     // Temporary list of users for testing, TODO: add database of users and chat messages
-    private List<User> users;
-    private List<ChatMessage> messages;
+    private ObservableList<User> users = FXCollections.emptyObservableList();
+    private HashMap<Integer, ObservableList<ChatMessage>> messages = new HashMap<>();
 
+    public ChatController() {
+        fillWithDummyData();
 
-    private void openChatMessage(int userId) {
+        showUsers();
+        showChatMessages(0);
+    }
+
+    private void fillWithDummyData() {
+        //users.add(new User(0, "Me, myself, and I"));
+
+        ObservableList<ChatMessage> msgs = FXCollections.observableArrayList(
+                new ChatMessage("TEST")
+        );
+
+        messages.put(0, msgs);
+    }
+
+    private void showUsers() {
+        //user_list_view.setItems(users);
+    }
+
+    private void showChatMessages(int userId) {
+        //message_list_view.setItems(messages.get(userId));
+    }
+
+    @FXML
+    protected void addChatMessage() {
+        //messages.get(0).add(new ChatMessage(chat_text.getText()));
     }
 
 }
