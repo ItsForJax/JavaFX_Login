aa9792b000ca1e7fa78395b3887a4af4d68ba6e9
user <email>
2024-05-29 15:58:26 +0800

    Added ChatHolders(list cells)

--- src/main/java/com/epds/javafx_login/utilities/controllers/ChatController.java
+++ src/main/java/com/epds/javafx_login/utilities/controllers/ChatController.java
@@ -2,10 +2,13 @@ package com.epds.javafx_login.utilities.controllers;
 
 import com.epds.javafx_login.entities.ChatMessage;
 import com.epds.javafx_login.entities.User;
+import com.epds.javafx_login.ui.ChatMessageCellController;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
@@ -43,7 +46,6 @@ public class ChatController {
         showChatMessages(0);
     }
 
-    @FXML
     private void fillWithDummyData() {
         users.add(new User(0, "Me, myself, and I"));
 
@@ -75,6 +77,35 @@ public class ChatController {
 
         if (!text.isEmpty())
             messages.get(currentId).add(new ChatMessage(chat_text.getText()));
+    }
+
+    private class ChatMessageListCell extends ListCell<ChatMessage> {
+
+        private AnchorPane root;
+        private ChatMessageCellController controller;
+
+        public ChatMessageListCell() {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("list_cells/chat_message_cell.fxml"));
+                root = loader.getRoot();
+                controller = loader.getController();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        protected void updateItem(ChatMessage message, boolean empty) {
+            super.updateItem(message, empty);
+
+            if (empty || message == null) {
+                setGraphic(null);
+            }
+            else {
+                controller.setChatMessageText(message);
+                setGraphic(root);
+            }
+        }
     }
 
 }
