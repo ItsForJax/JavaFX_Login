034c1202f4f8549f1bbc0acea43144c4a4412eac
jonathandavid723 <108653099+jonathandavid723@users.noreply.github.com>
2024-05-31 13:25:38 +0800

    Designed the cells for the User list and Chat Message List

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,11 +4,12 @@ module com.epds.javafx_login {
     requires java.sql;
     requires org.xerial.sqlitejdbc;
 
-
-    opens com.epds.javafx_login to javafx.fxml;
     exports com.epds.javafx_login;
+    opens com.epds.javafx_login to javafx.fxml;
     exports com.epds.javafx_login.utilities;
     opens com.epds.javafx_login.utilities to javafx.fxml;
     exports com.epds.javafx_login.utilities.controllers;
     opens com.epds.javafx_login.utilities.controllers to javafx.fxml;
+    exports com.epds.javafx_login.ui;
+    opens com.epds.javafx_login.ui to javafx.fxml;
 }
