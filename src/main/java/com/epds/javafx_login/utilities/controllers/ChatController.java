cdafb89a3587884a6f3a7cd538c06d7fdd502e51
user <email>
2024-05-31 13:25:38 +0800

    Designed the cells for the User list and Chat Message List

--- src/main/java/com/epds/javafx_login/utilities/controllers/ChatController.java
+++ src/main/java/com/epds/javafx_login/utilities/controllers/ChatController.java
@@ -3,6 +3,7 @@ package com.epds.javafx_login.utilities.controllers;
 import com.epds.javafx_login.entities.ChatMessage;
 import com.epds.javafx_login.entities.User;
 import com.epds.javafx_login.ui.ChatMessageCellController;
+import com.epds.javafx_login.ui.ChatUserCellController;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -13,6 +14,9 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.HashMap;
 
 public class ChatController {
@@ -41,6 +45,9 @@ public class ChatController {
     @FXML
     private void initialize() {
         send_chat_button.setOnAction(evt -> addChatMessage());
+
+        user_list_view.setCellFactory(userListView -> new ChatUserListCell(userListView));
+        chat_list_view.setCellFactory(chatMessageListView -> new ChatMessageListCell(chatMessageListView));
 
         fillWithDummyData();
 
@@ -81,16 +88,67 @@ public class ChatController {
             messages.get(currentId).add(new ChatMessage(chat_text.getText()));
     }
 
+    // ListCells for displaying Users and Chat Messages
+    private class ChatUserListCell extends ListCell<User> {
+        private AnchorPane root;
+        private ChatUserCellController controller;
+        private ListView<User> parent;
+
+        public ChatUserListCell(ListView<User> parent) {
+            this.parent = parent;
+
+            try {
+                final String CHAT_USER_CELL_FXML_PATH = "/com/epds/javafx_login/list_cells/chat_user_cell.fxml";
+
+                URL fxml = getClass().getResource(CHAT_USER_CELL_FXML_PATH);
+                FXMLLoader loader = new FXMLLoader(fxml);
+
+                // Load the fxml file
+                root = loader.load();
+                controller = loader.getController();
+
+                // Adjusts the width of each cell based on its parent, subtracting 16 due to padding
+                root.prefWidthProperty().bind(parent.widthProperty().subtract(16));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        protected void updateItem(User user, boolean empty) {
+            super.updateItem(user, empty);
+
+            if (empty || user == null) {
+                setGraphic(null);
+            }
+            else {
+                controller.chat_user_name.setText(user.getName());
+                setGraphic(root);
+            }
+        }
+    }
+
     private class ChatMessageListCell extends ListCell<ChatMessage> {
 
         private AnchorPane root;
         private ChatMessageCellController controller;
+        private ListView<ChatMessage> parent;
 
-        public ChatMessageListCell() {
+        public ChatMessageListCell(ListView<ChatMessage> parent) {
+            this.parent = parent;
+
             try {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("list_cells/chat_message_cell.fxml"));
-                root = loader.getRoot();
+                final String CHAT_MESSAGE_CELL_FXML_PATH = "/com/epds/javafx_login/list_cells/chat_message_cell.fxml";
+
+                URL resource = getClass().getResource(CHAT_MESSAGE_CELL_FXML_PATH);
+                FXMLLoader loader = new FXMLLoader(resource);
+
+                // Load the fxml file
+                root = loader.load();
                 controller = loader.getController();
+
+                // Adjusts the width of each cell based on its parent, subtracting 16 due to padding
+                root.prefWidthProperty().bind(parent.widthProperty().subtract(16));
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
@@ -104,7 +162,7 @@ public class ChatController {
                 setGraphic(null);
             }
             else {
-                controller.setChatMessageText(message);
+                controller.chat_message_text.setText(message.getMessage());
                 setGraphic(root);
             }
         }
