7ca64924669d905e9f7c23098bffb25755fe1eea
ItsForJax <dadelos.ddls@gmail.com>
2024-06-04 15:49:10 +0800

    [temporary] semi-working chat

---
+++ src/main/java/com/epds/javafx_login/api/chat/model/TicketIdResponse.java
@@ -0,0 +1,12 @@
+package com.epds.javafx_login.api.chat.model;
+
+public class TicketIdResponse {
+    private String ticket_id;
+
+    public TicketIdResponse(String ticket_id) {
+        this.ticket_id = ticket_id;
+    }
+
+    // Getter and setter
+    // ...
+}
