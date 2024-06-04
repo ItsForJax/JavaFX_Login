9debaba907c2020fcc6c2059a5062984fd050a06
jonathandavid723 <108653099+jonathandavid723@users.noreply.github.com>
2024-06-04 15:19:13 +0800

    Chat can now accept data from the API

--- src/main/java/com/epds/javafx_login/api/chat/model/ChatItem.java
+++ src/main/java/com/epds/javafx_login/api/chat/model/ChatItem.java
@@ -2,21 +2,16 @@ package com.epds.javafx_login.api.chat.model;
 
 import com.google.gson.annotations.SerializedName;
 
-import java.util.Date;
-
 public class ChatItem {
 
     @SerializedName("isSupport")
-    private boolean is_support;
+    private boolean support;
 
     @SerializedName("msg_id")
     private int message_id;
 
     @SerializedName("msg")
     private String message;
-    public String getMessage() {
-        return message;
-    }
 
     @SerializedName("sender")
     private String sender_email;
@@ -24,10 +19,52 @@ public class ChatItem {
     @SerializedName("timestamp")
     private String timestamp;
 
+
+    // Getters and Setters
+    public boolean isSupport() {
+        return support;
+    }
+
+    public void setSupport(boolean support) {
+        this.support = support;
+    }
+
+    public int getMessage_id() {
+        return message_id;
+    }
+
+    public void setMessage_id(int message_id) {
+        this.message_id = message_id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSender_email() {
+        return sender_email;
+    }
+
+    public void setSender_email(String sender_email) {
+        this.sender_email = sender_email;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
     @Override
     public String toString() {
         return "ChatItem{" +
-                "is_support=" + is_support +
+                "is_support=" + support +
                 ", message_id=" + message_id +
                 ", message='" + message + '\'' +
                 ", sender_email='" + sender_email + '\'' +
--- src/main/java/com/epds/javafx_login/entities/User.java
+++ src/main/java/com/epds/javafx_login/entities/User.java
@@ -6,6 +6,7 @@ public class User {
 
     private String name;
 
+
     public User(int id, String name) {
         this.id = id;
         this.name = name;
